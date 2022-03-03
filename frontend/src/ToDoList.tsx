75597cbe9a7a41694812915ea89ce30ecd260b2a
philipp.weikert <philippweikert3@gmail.com>
2022-03-03 17:44:52 +0100

    withTranslate

--- frontend/src/ToDoList.tsx
+++ frontend/src/ToDoList.tsx
@@ -2,19 +2,22 @@ import {useState, useEffect} from "react"
 import {ToDo} from "./model";
 import ToDoElement from "./ToDoElement";
 import ToDoForm from "./ToDoForm";
+import {useTranslation} from "react-i18next";
 
 export default function ToDoList() {
+
+    const {t} = useTranslation();
 
     const [toDos, setToDos] = useState([] as Array<ToDo>)
 
     const fetchAll = () => {
-      fetch('http://localhost:8080/todolist')
+      fetch(`${process.env.REACT_APP_BASE_URL}/todolist`)
           .then(response => response.json())
           .then((toDosFromBackend: Array<ToDo>) => setToDos(toDosFromBackend));
     }
 
     const deleteChecked = () => {
-        fetch('http://localhost:8080/todolist' ,
+        fetch(`${process.env.REACT_APP_URL}/todolist` ,
             {method: 'DELETE'})
             .then(response => response.json())
             .then((toDosFromBackend : Array<ToDo>) => setToDos(toDosFromBackend));
@@ -30,7 +33,7 @@ export default function ToDoList() {
                 <ToDoForm onToDoCreation={setToDos} />
             </div>
             <div>
-                <button onClick={deleteChecked} >Arbeit ist vollbracht!</button>
+                <button onClick={deleteChecked} >{t('done-button')}</button>
             </div>
             <ul>
             {toDos.map(task => <li key={task.id}><ToDoElement toDoItem={task} onToDoDeletion={fetchAll} onToDoChange={setToDos}/></li>)}
